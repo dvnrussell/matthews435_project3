de13146f68889f0f469b570aae44efe8c7b419c0
Devon Russell <dvnrussell@gmail.com>
2020-04-25 20:01:41 -0500

    Added javadoc to classes

--- FileThreadManager.java
+++ FileThreadManager.java
@@ -2,15 +2,29 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+/**
+ * An object constructed with the name a file and contains methods for
+ * manipulating the contents of that file
+ * @author dvnrussell
+ */
 public class FileThreadManager {
 
     File textFile;
     final String FLAG = "%%EOF%%";
 
+    /**
+     * Constructor.
+     * 
+     * @param fileName name of the file to be used in the class's methods
+     */
     public FileThreadManager(String fileName) {
         this.textFile = new File(fileName);
     }
 
+    /**
+     * Takes each line of the class's file and pushes onto a buffer
+     * @param buffer the buffer that will accept all of the file's lines (FIFO)
+     */
     private void enqueueParsedLines(SynchronizedBuffer<String> buffer) {
         try {
             Scanner scnr = new Scanner(textFile);
@@ -33,6 +47,12 @@ public class FileThreadManager {
 
     }
 
+    /**
+     * Takes each string in a buffer and replaces all of the digits with the 
+     * phonetic spelling of that digit
+     * @param inputBuffer the buffer containing the strings to change
+     * @param outputBuffer the buffer where the changed strings will be stored
+     */
     private void replaceDigitsWithText(SynchronizedBuffer<String> inputBuffer,
         SynchronizedBuffer<String> outputBuffer) {
         try {
@@ -58,6 +78,11 @@ public class FileThreadManager {
         }
     }
 
+    /**
+     * Takes each string in a buffer and reverses all of the characters
+     * @param inputBuffer the buffer containing the strings to change
+     * @param outputBuffer the buffer where the reversed strings will be stored
+     */
     private void reverseLineCharacters(SynchronizedBuffer<String> inputBuffer,
         SynchronizedBuffer<String> outputBuffer) {
 
@@ -75,6 +100,10 @@ public class FileThreadManager {
         }
     }
 
+    /**
+     * Removes each string from a buffer and prints to the console
+     * @param buffer the buffer containing the strings to be printed
+     */
     private void printLinesFromBuffer(SynchronizedBuffer<String> buffer) {
 
         try {
@@ -88,6 +117,11 @@ public class FileThreadManager {
         }
     }
 
+    /**
+     * Starts the thread that will perform the file parsing function
+     * on a buffer
+     * @param buffer the buffer to pass to the file parsing function
+     */
     public synchronized void fileParseThreadInitializer(
         SynchronizedBuffer<String> buffer) {
 
@@ -95,6 +129,12 @@ public class FileThreadManager {
         t.start();
     }
 
+    /**
+     * Starts the thread that will perform the digit replacement function
+     * on a buffer
+     * @param inputBuffer the buffer with the strings to replace digits for
+     * @param outputBuffer the buffer where the new strings will be stored
+     */
     public synchronized void digitReplacementThreadInitializer(
         SynchronizedBuffer<String> inputBuffer, 
         SynchronizedBuffer<String> outputBuffer) {
@@ -104,6 +144,12 @@ public class FileThreadManager {
         t.start();
     }
 
+    /**
+     * Starts the thread that will perform the character reversal function
+     * on a buffer
+     * @param inputBuffer the buffer with the strings to reverse
+     * @param outputBuffer the buffer where the new strings will be stored
+     */
     public synchronized void characterReversalThreadInitializer(
         SynchronizedBuffer<String> inputBuffer,
         SynchronizedBuffer<String> outputBuffer) {
@@ -113,6 +159,10 @@ public class FileThreadManager {
         t.start();
     }
 
+    /**
+     * Starts the thread that will print each string in a buffer
+     * @param buffer
+     */
     public synchronized void linePrintingThreadInitializer(
         SynchronizedBuffer<String> buffer) {
 
--- SynchronizedBuffer.java
+++ SynchronizedBuffer.java
@@ -2,6 +2,10 @@ import java.util.ArrayList;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * Represents a buffer whose actions (add and remove) are performed atomically,
+ * allowing multiple threads to operate with one set of data
+ */
 public class SynchronizedBuffer<T> {
 
 
@@ -11,6 +15,10 @@ public class SynchronizedBuffer<T> {
     private ReentrantLock lock;
     private Condition notFull, notEmpty;
 
+    /**
+     * Constructor
+     * @param sizeLimit the maximum size of the buffer object
+     */
     public SynchronizedBuffer(int sizeLimit) {
         this.sizeLimit = sizeLimit;
         bufferList = new ArrayList<T>();
@@ -19,14 +27,27 @@ public class SynchronizedBuffer<T> {
         notEmpty = lock.newCondition();
     }
 
+    /**
+     * Checks if the number of items in the buffer is the same as the size limit
+     * @return true when the number of items equals the buffer's size limit
+     */
     private boolean isFull() {
         return bufferList.size() == sizeLimit;
     }
 
+    /**
+     * Checks if the number of items in the buffer is 0
+     * @return true when the number of items equals 0
+     */
     private boolean isEmpty() {
         return bufferList.size() == 0;
     }
 
+    /**
+     * Adds an item to the buffer atomically
+     * @param item the object/data to be added to the buffer
+     * @throws InterruptedException throws if the atomic action is interrupted
+     */
     public void add(T item) throws InterruptedException {
         lock.lock();
         try {
@@ -44,6 +65,12 @@ public class SynchronizedBuffer<T> {
         lock.unlock();
     }
 
+    /**
+     * Removes an item from the buffer atomically and returns it
+     * @param item the object/data to be removed from the buffer
+     * @return the item removed from the buffer
+     * @throws InterruptedException throws if the atomic action is interrupted
+     */
     public T remove() throws InterruptedException {
         lock.lock();
         T item = null;
